2f50359fe42790d45c613e35c0683ff87c403665
Alex Tomko <tomko.alex@gmail.com>
2019-08-14 17:22:11 +0300

    Initial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fsof6-1.fna.fbcdn.net/vp/e14cbc5bce72f86dfdb8989c709b507d/5DDBFE38/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fsof6-1.fna.fbcdn.net" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>153</strong>posts</div>
+                <div class="pr-4"><strong>23K</strong>followers</div>
+                <div class="pr-4"><strong>22</strong>following</div>
+            </div>
+            <div>freeCodeCamp.org</div>
+        </div>
+    </div>
+</div>
+@endsection
